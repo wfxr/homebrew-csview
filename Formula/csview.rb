4913621744af5e403f3cee3464b62ef02f913e78
Wenxuan Zhang <wenxuangm@gmail.com>
2021-03-19 20:25:37 +0800

    bump up version

--- Formula/csview.rb
+++ Formula/csview.rb
@@ -4,10 +4,10 @@
 class Csview < Formula
   desc "A high performance csv viewer with cjk/emoji support."
   homepage "https://github.com/wfxr/csview"
-  url "https://github.com/wfxr/csview/releases/download/v0.3.6/csview-v0.3.6-x86_64-apple-darwin.tar.gz"
-  sha256 "074566b018ad6d79948d83353160cde9072eefc16a2a3c3fa7785a986e5202d3"
+  url "https://github.com/wfxr/csview/releases/download/v0.3.7/csview-v0.3.7-x86_64-apple-darwin.tar.gz"
+  sha256 "86bf1f22b0cdd0a753bf5d46467bcd784901af13cf70f30fbe6f965205407da7"
   license "MIT/APACHE-2.0"
-  version "0.3.6"
+  version "0.3.7"
 
   def install
     bin.install "csview"
